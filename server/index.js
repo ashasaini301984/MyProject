1b468c1f6c5ba4bcd3c7c695bb453083261f4810
ashasaini301984 <ashasaini301984@gmail.com>
2025-07-28 02:52:26 -0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const cors=require('cors');
 
-const port = 3001;
+const port = 'https://frontend-8lri.onrender.com';
 const app = express();
 app.use(express.json());
 app.use(cors());
